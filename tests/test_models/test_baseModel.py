fef7dc5df179f094bdafe3d928787dd7a9878d07
klitscher <672@holbertonschool.com>
2019-07-03 20:44:09 +0000

    Reverting

--- tests/test_models/test_baseModel.py
+++ tests/test_models/test_baseModel.py
@@ -1,79 +1,122 @@
-"""Test module for BaseModel class"""
+#!/usr/bin/python3
+"""Module to test BaseModel class"""
 
 
+from datetime import datetime, timedelta
 import unittest
-import datetime
+import io
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-import os
-import os.path
-import uuid
+from contextlib import redirect_stdout
+from time import sleep
 
 
-class TestBase(unittest.TestCase):
-    """Tests for the BaseModel class"""
+class TestBaseModel(unittest.TestCase):
+    """Tests for BaseModel class"""
 
-    def __init__(self, *args, **kwargs):
-        """Method to set up data model to test"""
-
-        super().__init__(*args, **kwargs)
-        self._class = BaseModel
+    def setUp(self):
+        """Set up which instance to call"""
+        self._class = BaseModel()
+        self._class2 = BaseModel()
         self._name = "BaseModel"
 
-    def test_id(self):
-        """Test base model UUID is created properly"""
+    # -------------Tests Public Attributes-------------------------------
 
-        base = self._class()
-        with self.subTest(msg="id is a UUID"):
-            self.assertIsInstance(base.id, str)
-            self.assertIsInstance(uuid.UUID(base.id), uuid.UUID)
-        with self.subTest(msg="IDs are unique"):
-            self.assertNotEqual(self._class().id, self._class().id)
+    def test_idCorrect(self):
+        """Test that id is a unique string"""
+        my_model1 = self._class
+        my_model2 = self._class2
+        self.assertTrue((type(my_model1.id) is str))
+        self.assertNotEqual(my_model1.id, my_model2.id)
 
-    def test_timeUpdate(self):
-        """Test that the time is set and updated correctly"""
+    def test_createUpdateType(self):
+        """Test that create and update are datetime objects"""
+        my_model = self._class
+        self.assertTrue(isinstance(my_model.created_at, datetime))
+        self.assertTrue(isinstance(my_model.updated_at, datetime))
 
-        base = self._class()
-        with self.subTest(msg="Creation time"):
-            now = datetime.datetime.now()
-            self.assertIsInstance(base.updated_at, datetime.datetime)
-            self.assertTrue(0 < (now - base.updated_at).total_seconds() < 1)
-        with self.subTest(msg="Updated time"):
-            old = base.updated_at
-            base.save()
-            now = datetime.datetime.now()
-            self.assertTrue(old < base.updated_at < now)
+    def test_updateDif(self):
+        """Test that update is different than create after update"""
+        my_model = self._class
+        self.assertEqual(my_model.created_at.second,
+                         my_model.updated_at.second)
+        s1 = my_model.created_at.second
+        sleep(1)
+        my_model.name = "Holberton"
+        s2 = my_model.updated_at.second
+        self.assertEqual(s1, s2)
 
-    def test_str(self):
-        """Test __str__ method in BaseModel class"""
+    # ---------Test Save Method --------------------------------
 
-        base = self._class()
-        yes = "[" + self._name + "] ({}) {}".format(
-            base.id, str(base.__dict__))
-        self.assertEqual(str(base), yes)
+    def test_saveCorrect(self):
+        """Tests that save works correctly"""
+        my_model = self._class
+        s1 = my_model.created_at.second
+        sleep(1)
+        my_model.save()
+        s2 = my_model.updated_at.second
+        if (s2 == 0):
+            s2 = 60
+        self.assertEqual(s1, s2 - 1)
 
-    def test_createTime(self):
-        """Tests creation time stamp for BaseModel"""
+    def test_saveArgs(self):
+        """Tests passing args to save"""
+        my_model = self._class
+        self.assertRaises(TypeError, my_model.save, "Test")
 
-        base = self._class()
-        now = datetime.datetime.now()
-        self.assertIsInstance(base.created_at, datetime.datetime)
-        self.assertTrue(0 < (now - base.created_at).total_seconds() < 1)
+    # ---------Test to_dict Method ----------------------------
 
-    def test_toDict(self):
-        """Test converting to dictionary with to_dict"""
+    def test_toDictCorrect(self):
+        """Tests that it works"""
+        my_model = self._class
+        dic = my_model.to_dict()
+        flag = True
+        for key, value in my_model.__dict__.items():
+            if key not in dic:
+                flag = False
+        self.assertTrue(flag)
 
-        base = self._class()
-        dic = base.to_dict()
-        with self.subTest(msg="Has all attributes"):
-            a = set(dic.keys())
-            b = set(base.__dict__.keys())
-            self.assertTrue(b.issubset(a))
-        with self.subTest(msg="class name added"):
-            self.assertTrue("__class__" in dic.keys())
-            self.assertEqual(dict["__class__"], self._name)
-        with self.subTest(msg="times converted to string"):
-            self.assertIsInstance(dic["created_at"], str)
-            self.assertIsInstance(dic["updated_at"], str)
-            self.assertEqual(dic["created_at"], base.created_at.isoformat())
-            self.assertEqual(dic["updated_at"], base.updated_at.isoformat())
+    def test_toDictClass(self):
+        """Tests that __class__ was added"""
+        my_model = self._class
+        dic = my_model.to_dict()
+        flag = True
+        for key, value in dic.items():
+            if "__class__" not in dic:
+                flag = False
+        self.assertTrue(flag)
+
+    def test_toDictCreatedFormat(self):
+        """Tests that created_at is in correct format"""
+        my_model = self._class
+        correct = datetime.isoformat(my_model.created_at)
+        dic = my_model.to_dict()
+        flag = True
+        if correct not in dic.values():
+                flag = False
+        self.assertTrue(flag)
+
+    def test_toDictUpdatedFormat(self):
+        """Tests that updated_at is in correct format"""
+        my_model = self._class
+        correct = datetime.isoformat(my_model.updated_at)
+        dic = my_model.to_dict()
+        flag = True
+        if correct not in dic.values():
+                flag = False
+        self.assertTrue(flag)
+
+    def test_toDictArgs(self):
+        """Tests passing args to to_dict"""
+        my_model = self._class
+        self.assertRaises(TypeError, my_model.to_dict, "Test")
+
+    # ---------Test __str__ Method ---------------------------------
+
+    def test_strCorrect(self):
+        """Test that str works correctly"""
+        my_model = self._class
+        f = io.StringIO()
+        s = "[{}] ({}) {}\n".format(self._name, my_model.id, my_model.__dict__)
+        with redirect_stdout(f):
+            print(my_model)
+        self.assertEqual(f.getvalue(), s)
